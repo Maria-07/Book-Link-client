6ff897de91e6659daaf578bcf02e6e1556548d95
Shanjida Rahman Maria <shanjidamaria07@gmail.com>
2023-07-14 19:52:28 +0600

    Project Pages route set up done

---
+++ src/pages/Books/Books.tsx
@@ -0,0 +1,5 @@
+const Books = () => {
+  return <div>ALl Books Here</div>;
+};
+
+export default Books;
---
+++ src/routes/routes.tsx
@@ -0,0 +1,48 @@
+import { createBrowserRouter } from 'react-router-dom';
+import App from '../App';
+import Home from '../pages/Home/Home';
+import Books from '../pages/Books/Books';
+import BookDetails from '../pages/BookDetails/BookDetails';
+import WishList from '../pages/WishList/WishList';
+import Login from '../pages/Login/Login';
+import SignUp from '../pages/SignUp/SignUp';
+import NotFound from '../pages/NotFound';
+
+const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/books',
+        element: <Books />,
+      },
+      {
+        path: '/books/:id',
+        element: <BookDetails />,
+      },
+      {
+        path: '/wish-list',
+        element: <WishList />,
+      },
+    ],
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <SignUp />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
+
+export default routes;
